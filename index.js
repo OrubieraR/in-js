082f49ecd2c4ccf107ebf0cfa8aa8acc534e3eee
Octavio <octavio.rubiera@correos.com>
2022-07-27 01:32:30 +0200

    Añadido el código hasta los resultados de los cuartos de final

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 /*Information in variables*/
-    let playoffStartMsg=`====================================================
-    === COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===
-    ====================================================`;
+let playoffStartMsg=`====================================================
+=== COMIENZAN LAS FASES ELIMINATORIAS DEL TORNEO ===
+====================================================`;
+let teams4PlayoffMsg, quarterTeamsMsg, quarterGoals, semiTeamsMsg, consolTeamsMsg, finalTeamsMsg, winnerMsg;
+
 
 /*Playoffs initial teams*/
 const initGroups=[['Inglaterra','Austria','Noruega','Irlanda'],['Alemania','Dinamarca','España','Finlandia'],['Holanda','Suecia','Rusia','Suiza'],['Francia','Italia','Bélgica','Islandia']];
 const initGroupsLength=initGroups.length;
 const fakeIndex=[];
 const playoffsTeams=[];
+const quarterTeams=[];
+const quarterWinnerTeams=[];
+const semiTeams=[];
+const consolTeams=[];
+const finalTeams=[];
 let num, num2, tempArray;
 
 // Fake index for random teams
 for (let i = 0; i < initGroupsLength; i++) {
     num=randomNumber(4);
     tempArray=[num];
-    /*Junto con la función devuelve undefined
-    num2=getNewNumber(num);*/
     num2=randomNumber(4);
     if (num===num2) {
         do{
@@ -44,19 +49,111 @@ function randomNumber(max){
     return Math.floor(Math.random() * max);
 }
 
-// Así devuelve undefined si hay un número repetido
-/*function getNewNumber(a){
-    newNumber=randomNumber(2);
-    if (a===newNumber) {
-        getNewNumber(num);
+// Generating goals for the matches
+function goals(a,b){
+    const allGoals=[];
+    
+    for (let i = 0; i < a; i++) {
+        const pairGoals=[];
+        // if (i%2===0) {
+            pairGoals.push(randomNumber(b),randomNumber(b));
+            allGoals.push(pairGoals);
+        // }
     }
-    else{
-        return newNumber;
-    }
-}*/
+    return allGoals;
+}
 
 
 // Worldcup knockout stages begin
 console.log(playoffStartMsg);
-console.log(fakeIndex);
-console.log(playoffsTeams);
+
+// Selecting and showing 8 playoffs teams
+// console.log(fakeIndex);
+// console.log(playoffsTeams);
+
+teams4PlayoffMsg=`
+Los equipos que participarán en el playoff son:
+    GRUPO A: ${playoffsTeams[0][0]}, ${playoffsTeams[0][1]}
+    GRUPO B: ${playoffsTeams[1][0]}, ${playoffsTeams[1][1]}
+    GRUPO C: ${playoffsTeams[2][0]}, ${playoffsTeams[2][1]}
+    GRUPO D: ${playoffsTeams[3][0]}, ${playoffsTeams[3][1]}
+`;
+console.log(teams4PlayoffMsg);
+
+
+// Results of quarter final matches and teams
+for (let i = 0; i < playoffsTeams.length; i++) {
+    for (let j = 0; j < playoffsTeams[i].length; j++) {
+        let tempArray=[];
+        if (i%2 === 0) {
+            if (j%2 === 0) {
+                // tempArray+=`${playoffsTeams[i][j]} - ${playoffsTeams[i+1][j+1]} => Winner`;
+                tempArray.push(playoffsTeams[i][j],playoffsTeams[i+1][j+1]);
+                quarterTeams.push(tempArray);
+            }
+        }
+        else{
+            if (j%2 === 0) {
+                // tempArray+=`${playoffsTeams[i][j]} - ${playoffsTeams[i-1][j+1]} => Winner`;
+                tempArray.push(playoffsTeams[i][j],playoffsTeams[i-1][j+1]);
+                quarterTeams.push(tempArray);
+            }
+        }
+    }
+}
+
+quarterGoals=goals(quarterTeams.length,2);
+
+// Calculating winners and semifinals teams
+for (let i = 0; i < quarterGoals.length; i++) {
+    let difGoals;
+    for (let j = 0; j < quarterGoals[i].length; j++) {
+        if (j === 0) {
+            difGoals=quarterGoals[i][j]-quarterGoals[i][j+1];
+    
+            if (difGoals>0) {
+                quarterWinnerTeams.push(quarterTeams[i][0]);
+            }
+    
+            else if(difGoals===0){
+                let tempGoals = randomNumber(3);
+                let tempGoals2= randomNumber(3);
+                if (tempGoals===tempGoals2) {
+                    do{
+                        tempGoals2=randomNumber(3);
+                    }
+                    while(tempGoals===tempGoals2);
+
+                    if (tempGoals>tempGoals2) {
+                        quarterWinnerTeams.push(quarterTeams[i][0]+' (Penalties: '+tempGoals+' - '+ tempGoals2+')');
+                    }
+                    else{
+                        quarterWinnerTeams.push(quarterTeams[i][1]+' (Penalties: '+tempGoals+' - '+ tempGoals2+')');
+                    }
+                }
+                else{
+                    if (tempGoals>tempGoals2) {
+                        quarterWinnerTeams.push(quarterTeams[i][0]+' (Penalties: '+tempGoals+' - '+ tempGoals2+')');
+                    }
+                    else{
+                        quarterWinnerTeams.push(quarterTeams[i][1]+' (Penalties: '+tempGoals+' - '+ tempGoals2+')');
+                    }
+                }
+            }
+            
+            else {
+                quarterWinnerTeams.push(quarterTeams[i][1]);
+            }
+        }
+    }
+    
+}
+
+quarterTeamsMsg=`
+===== CUARTOS DE FINAL =====
+    ${quarterTeams[0][0]} ${quarterGoals[0][0]} - ${quarterGoals[0][1]} ${quarterTeams[0][1]} => ${quarterWinnerTeams[0]}
+    ${quarterTeams[1][0]} ${quarterGoals[1][0]} - ${quarterGoals[1][1]} ${quarterTeams[1][1]} => ${quarterWinnerTeams[1]}
+    ${quarterTeams[2][0]} ${quarterGoals[2][0]} - ${quarterGoals[2][1]} ${quarterTeams[2][1]} => ${quarterWinnerTeams[2]}
+    ${quarterTeams[3][0]} ${quarterGoals[3][0]} - ${quarterGoals[3][1]} ${quarterTeams[3][1]} => ${quarterWinnerTeams[3]}
+`;
+console.log(quarterTeamsMsg);
